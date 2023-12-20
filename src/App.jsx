6691545dabc016dd42bbaa747c66548a2de52dbb
Shivam Jha <83356798+2008shivamjha@users.noreply.github.com>
2023-12-20 21:02:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+    name: "Shivam Jha",
+  title: "Malware Analyst",
+  email: "jhashivam2008@gmail.com",
+  gitHub: "github.com/2008shivamjha",
+  instagram: "2008_shivam_jha",
+  linkedIn: "https://www.linkedin.com/in/shivam-jha-aa2974208/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "NA",
+  youTube: "NA",
 };
 
 const primaryColor = "#4E567E";
